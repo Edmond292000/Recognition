656941906a257ec438620be5a5d55904bfbf7d24
Edmond292000 <yeehang2920@gmail.com>
2025-09-18 13:11:51 +1000

    built BasicBlock1 and BasicBlock2 for the encoder

--- vae.py
+++ vae.py
@@ -10,3 +10,35 @@ from torch.utils.data import DataLoader
 import time
 import matplotlib.pyplot as plt
 
+
+# Encoder
+# ---- Conv -> Relu -> Pool -> BN ----
+class BasicBlock1(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride):
+        super(BasicBlock1, self).__init__()
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                              stride=stride)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.bn = nn.BatchNorm2d(num_features=out_channels)
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = F.relu(out)
+        out = self.pool(out)
+        out = self.bn(out)
+        return out
+
+
+# ---- Conv -> Relu -> BN ----
+class BasicBlock2(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride):
+        super(BasicBlock2, self).__init__()
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                              stride=stride)
+        self.bn = nn.BatchNorm2d(num_features=out_channels)
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = F.relu(out)
+        out = self.bn(out)
+        return out
