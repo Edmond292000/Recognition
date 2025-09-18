24cbdd8c34fea636f524a54ef250c26354a2f3a7
Edmond292000 <yeehang2920@gmail.com>
2025-09-18 22:22:10 +1000

    built Encoder class

--- vae.py
+++ vae.py
@@ -10,6 +10,9 @@ from torch.utils.data import DataLoader
 import time
 import matplotlib.pyplot as plt
 
+# hyperparameter
+latent_dim = 256
+
 
 # Encoder
 # ---- Conv -> Relu -> Pool -> BN ----
@@ -42,3 +45,48 @@ class BasicBlock2(nn.Module):
         out = F.relu(out)
         out = self.bn(out)
         return out
+
+
+class Encoder(nn.Module):
+    def __init__(self, latent_dim):
+        super(Encoder, self).__init__()
+
+        # layer 1
+        self.layer1 = BasicBlock1(1, 96, 5, 1)
+
+        # layer 2
+        self.layer2 = BasicBlock2(96, 128, 3, 1)
+
+        # layer 3
+        self.layer3 = BasicBlock1(128, 192, 3, 1)
+
+        # layer 4
+        self.layer4 = BasicBlock2(192, 256, 3, 1)
+
+        # fully connected
+        self.flatten = nn.Flatten()
+        self.fc = nn.Linear(256 * 11 * 11, 4096)
+        self.fc_mu = nn.Linear(4096, latent_dim)
+        self.fc_logvar = nn.Linear(4096, latent_dim)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.flatten(x)
+        h = F.relu(self.fc(x))
+
+        mu = self.fc_mu(h)
+        logvar = self.fc_logvar(h)  # log(var^2)
+
+        return mu, logvar
+
+    def reparameterization(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return mu + std * eps
+
+
+
